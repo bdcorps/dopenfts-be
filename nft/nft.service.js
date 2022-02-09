d553adb7fa4a398252976710de6a5af9bf717933
shaan <hashmishaana@gmail.com>
2022-02-08 21:51:16 -0500

    Updated the DM to include the number of times shown to user

--- nft/nft.service.js
+++ nft/nft.service.js
@@ -1,11 +1,19 @@
-const addNFT = (NFT) => (id, name, image, wallet_address, likes) => {
-  if (!id || !name || !image || !wallet_address || !likes)
-    throw new Error(
-      "Missing Data. Please provide values for id, name, image, wallet_address, and likes."
-    );
-  const NFT = new NFT({ id, name, image, wallet_address, likes });
-  return NFT.save();
-};
+const addNFT =
+  (NFT) => (id, name, image, wallet_address, likes, shown_to_user) => {
+    if (!id || !name || !image || !wallet_address || !likes || !shown_to_user)
+      throw new Error(
+        "Missing Data. Please provide values for id, name, image, wallet_address, and likes."
+      );
+    const NFT = new NFT({
+      id,
+      name,
+      image,
+      wallet_address,
+      likes,
+      shown_to_user,
+    });
+    return NFT.save();
+  };
 
 const listNFT = (NFT) => () => {
   return NFT.find({});
